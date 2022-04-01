b1f23dbe0daee7ca5a42e9bbdc216b9f86c0698b
Norman Shipman <norman@nshipman.io>
2022-04-01 07:52:54 -0400

    updating tf backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "nshipman-io-terraform-state"
+    key = "acg-python-aws-etl/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
